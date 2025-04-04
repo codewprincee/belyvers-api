afbb843e42879d97d53c4e3fe8387155243d2a25
Prince kumar Singh <38320278+iprincekumarsingh@users.noreply.github.com>
2025-04-05 02:46:23 +0530

    c

--- src/index.ts
+++ src/index.ts
@@ -76,7 +76,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   connectToMongoDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT || 3000, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
